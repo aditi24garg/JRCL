6cb16f96fdb59138232c4587aa121689ba3a56c7
aditi24garg <58719176+aditi24garg@users.noreply.github.com>
2026-01-19 05:35:12 +0000

    error

--- iron_App.py
+++ iron_App.py
@@ -49,13 +49,6 @@ with output_col:
 
             result=process_stock(stock_length,requirements,diameter=stock_diameter)
             pdf_data.append(result)
-            #pdf_buffer = generate_pdf_report(client_name, stock_requirements, pdf_data)
-            st.download_button(
-               label="Download PDF Report",
-               data=pdf_buffer,
-               file_name=f"{client_name}_iron_report.pdf",
-               mime="application/pdf"
-               )
 
             st.markdown(f"**No. of Bars to Order:** {result['bars_used']}")
             st.markdown(f"**Total Weight of Bars:** {result['total_weight_used'] + result['total_weight_wasted']:.2f} kg")
@@ -76,5 +69,12 @@ with output_col:
             if result['unfulfilled_pieces']>0:
                st.warning(f"Unfulfilled pieces: {result['unfulfilled_pieces']} (cannot fit in bars)")
 
+            pdf_buffer = generate_pdf_report(client_name, stock_requirements, pdf_data)
+            st.download_button(
+               label="Download PDF Report",
+               data=pdf_buffer,
+               file_name=f"{client_name}_iron_report.pdf",
+               mime="application/pdf")
+               
             
 
